1778ed27ef5d6f31c577cf17bb12a494f2c0b9a9
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-07-21 14:07:25 +0900

    insertionSort

--- insertionSort/insertionSort.playground/Contents.swift
+++ insertionSort/insertionSort.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var insertionSortArray: [Int] = [56, 17, 63, 34, 77, 52, 68]
+var j = Int()
+
+func insertionSortAlgorithm(array: inout [Int]) {
+    for i in 1..<array.count {
+        let key = array[i]
+        var j = i - 1
+
+        while j >= 0 && array[j] > key {
+            array[j + 1] = array[j]
+            j -= 1
+        }
+        array[j + 1] = key
+    }
+}
+
+insertionSortAlgorithm(array: &insertionSortArray)
+print(insertionSortArray)
